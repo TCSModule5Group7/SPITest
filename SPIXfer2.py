4f14f31b9d597c4ceb20a71e3483a80fbc7dfb03
killera40 <arwin.sleutjes@gmail.com>
2017-11-07 14:16:30 +0100

    change the 32 bit version of the test code

--- SPIXfer2.py
+++ SPIXfer2.py
@@ -5,7 +5,10 @@ from time import sleep
 byteArray = [None] * 0b11111111
 
 for i in range(0b00000000, 0b11111111):
-    byteArray[i] = i
+    if i % 2 ==0:
+        byteArray[i] = 0x0F
+    else:
+        byteArray[i] = 0xF0
 
 transfer = 0b00000000
 read = 0b10000000
@@ -15,7 +18,7 @@ nop = 0b11000000
 spi = spidev.SpiDev()
 spi.open(0, 0)
 spi.mode = 0b00
-spi.max_speed_hz = 7629
+spi.max_speed_hz = 31200000
 def bytestohex(bytes):
     result = []
     for byte in bytes:
@@ -24,13 +27,18 @@ def bytestohex(bytes):
 
 def loop():
     try:
+        temp = [0x00, 0x0F]
+        resp = spi.xfer2(temp)
+        print("message: " + str(bytestohex(temp)) + " | response: " + str(bytestohex(resp)))
+        """
         while True:
             print("loop")
             for b in byteArray:
-                temp = [transfer, b]
+                temp = [transfer, 0xf0]
                 resp = spi.xfer2(temp)
                 print("message: " + str(bytestohex(temp)) +  " | response: " + str(bytestohex(resp)))
                 sleep(0.5)
+        """
     except KeyboardInterrupt:
         spi.close()
 
--- SPIXfer32bits.py
+++ SPIXfer32bits.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python
 import spidev
 
-op_tx = 0b00000000
+op_tx = 0b00000001
 no_data = 0b00000000
 
 four_byte_array = [op_tx, no_data, no_data, no_data]
 
 spi = spidev.SpiDev()
 spi.open(0, 0)
-spi.mode = 0b00
+spi.mode = 0b01
 spi.max_speed_hz = 125000000
-
+iterations = 24
+errors = 0
 if __name__ == "__main__":
     try:
-        while True:
+        while iterations >= 0:
             response = spi.xfer2(four_byte_array)
-            print(bin(response[0]) + "|" + bin(response[1]) + "|" + bin(response[2]) + "|" + bin(response[3]))
-    except KeyboardInterrupt:
+            print(format(response[0], '#010b') + "|" + format(response[1], '#010b') + "|" + format(response[2], '#010b') + "|" + format(response[3], '#010b'))
+            iterations -= 1
+            if response[3] == 0:
+                errors += 1
+    finally:
+        print(errors)
         spi.close()
 
